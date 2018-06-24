ecbcd307f7b4496faa954215644cfb17e9a752fc
parkerziegler <parker.ziegler@formidable.com>
2018-06-23 18:16:23 -0700

    Add async await logic for starting server.

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const graphqlHttp = require("express-graphql");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const fetch = require("isomorphic-fetch");
+require("es6-promise").polyfill();
+
+const start = async () => {
+  // make the req to RawGit for Formidable dogs
+  const res = await fetch("https://rawgit.com/FormidableLabs/dogs/master/dogs.json");
+
+  const data = await res.json();
+  const dogs = data.map((dog) => ({ ...dog,
+    likes: 0 }));
+
+  app.use(cors());
+
+  const { schema, context } = require("./schema");
+
+  const PORT = 3001;
+
+  const initializedGraphQLMiddleware = graphqlHttp({
+    // GraphQL’s data schema
+    schema: schema(dogs),
+    // Pretty Print the JSON response
+    pretty: true,
+    // Enable GraphiQL dev tool
+    graphiql: true,
+    // A function that returns extra data available to every resolver
+    context
+  });
+
+  app.use(initializedGraphQLMiddleware);
+
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+};
+
+start();
---
+++ src/schema.js
@@ -0,0 +1,57 @@
+const { makeExecutableSchema } = require("graphql-tools");
+
+const typeDefs = `
+  type Query {
+    dogs: [Dog]
+    dog(key: ID!): Dog
+  }
+  type Mutation {
+    likeDog(key: ID!): Dog
+    likeAllDogs: [Dog]
+  }
+  type Dog {
+    key: String
+    name: String
+    breed: String
+    color: String
+    imageUrl: String
+    description: String
+    likes: Int
+  }
+`;
+
+const resolvers = (dogs) => ({
+  Query: {
+    dogs: (root, args, context) => dogs,
+    dog: (roots, args, context) => dogs.find((a) => a.key === args.key)
+  },
+  Mutation: {
+    likeDog: (root, args, context) => {
+      const dog = dogs.find((a) => a.key === args.key);
+      const idx = dogs.indexOf(dog);
+      const liked = {
+        ...dog,
+        likes: dog.likes + 1
+      };
+      dogs.splice(idx, 1, liked);
+      return liked;
+    },
+    likeAllDogs: (root, args, context) => {
+      dogs.forEach((dog) => {
+        dog.likes += 1;
+      });
+      return dogs;
+    }
+  }
+});
+
+module.exports = {
+  schema: (dogs) => makeExecutableSchema({
+    typeDefs,
+    resolvers: resolvers(dogs)
+  }),
+  context: (headers, secrets) => ({
+    headers,
+    secrets
+  })
+};
